7269e6b42450c70e6730edaefd51d15fc1f98af7
twakonig <twakonig@student.ethz.ch>
2020-03-26 11:54:51 +0100

    Setup, class structure

--- cmake-build-debug/CMakeFiles/target_tracking_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for target_tracking_node.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/theresa/catkin_vi/src/target_tracking
-# Build directory: /home/theresa/catkin_vi/src/target_tracking/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
